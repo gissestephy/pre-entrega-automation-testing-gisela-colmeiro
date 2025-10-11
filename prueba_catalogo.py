c5456cec13033f0f099b1c347b5a5f873ce94914
gissestephy <gisela.s.colmeiro@gmail.com>
2025-10-11 17:42:30 -0300

    Se agrega Catalogo y se mejora el codigo

--- prueba_catalogo.py
+++ prueba_catalogo.py
@@ -1,7 +1,6 @@
 # Navegación y Verificación del Catálogo
-
-from selenium import webdriver
-import time
+from selenium import webdriver 
+import time 
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
@@ -13,12 +12,18 @@ try:
     driver.find_element(By.ID, "user-name").send_keys("standard_user")
     driver.find_element(By.ID, "password").send_keys("secret_sauce")
     driver.find_element(By.ID, "login-button").click()
+    driver.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
     time.sleep(5)
 
     # validacion de la redireccion de la pagina 
     assert '/inventory.html' in driver.current_url
-    
-    # Verificación del catálogo
+
+    # Validacion del Titulo
+    print("Titulo:", driver.title)
+    assert driver.title == "Swag Labs"
+    time.sleep(2)
+
+    # Valida productos
     productos = driver.find_elements(By.CLASS_NAME, "inventory_item")    
     productos[0].find_element(By.TAG_NAME,"button").click()  # Agregar primer producto al carrito
     carrito = driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text
--- prueba_titulo.py
+++ prueba_titulo.py
@@ -4,14 +4,6 @@
 # Tecnologías a usar: Python, Pytest, Selenium WebDriver, Git y Github
 # Estructura: 2 archivos separados (test y funciones aux) con comentarios descriptivos y usar nomenclatura significativa para los elementos
 
-# Automatización de Login
-# 1. Navegar a la página de login de saucedemo.com
-# 2. Ingresar credenciales válidas (usuario: standard_user, contraseña: secret_sauce)
-# Validar login exitoso verificando que se haya redirigido a la página de inventario
-
-# Criterios mínimos de aceptación:
-# Login automatizado con espera explícita y validación de /inventory.html y "Products/Swag Labs".
-
 from selenium import webdriver
 import time 
 
